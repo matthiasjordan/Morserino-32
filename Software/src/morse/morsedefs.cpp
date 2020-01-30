48124b4cd6a9c85eed905053c29bcfa0a616fcdd
mj <mjordan@code-fu.de>
2020-01-30 21:27:38 +0100

    Less chatty default functions

--- Software/src/morse/morsedefs.cpp
+++ Software/src/morse/morsedefs.cpp
@@ -4,23 +4,19 @@
 void voidFunction()
 {
     // Well, nothing.
-    MORSELOGLN("voidFunction");
 }
 
 void voidFunction(String s) {
-    MORSELOGLN("voidFunction String");
 }
 
 
 unsigned long uLongFunctionMinus1()
 {
-    MORSELOGLN("uLongFunctionMinus1");
     return -1;
 }
 
 
 boolean booleanFunctionFalse()
 {
-    MORSELOGLN("booleanFunctionFalse");
     return false;
 }
