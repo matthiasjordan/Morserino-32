b6f364ff7c111af55f91d59372cbf1bc35a4aa90
mj <mjordan@code-fu.de>
2020-02-09 19:16:09 +0100

    GPL headers

--- Software/src/morse/WordBuffer.cpp
+++ Software/src/morse/WordBuffer.cpp
@@ -1,10 +1,16 @@
-/*
- * WordBuffer.cpp
+/******************************************************************************************************************************
+ *  morse_3 Software for the Morserino-32 multi-functional Morse code machine, based on the Heltec WiFi LORA (ESP32) module ***
+ *  Copyright (C) 2020  Matthias Jordan, DL4MAT
  *
- *  Created on: 30.01.2020
- *      Author: mj
- */
-
+ *  This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ *  of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with this program.
+ *  If not, see <https://www.gnu.org/licenses/>.
+ *****************************************************************************************************************************/
 #include "WordBuffer.h"
 
 WordBuffer::WordBuffer()
